eff21c658c8261bef5983fd9e0622a479f67976d
viniciusbg18 <viniciusbaganhagodoy@gmail.com>
2025-04-28 21:45:57 -0300

    Create EX7.cpp

---
+++ Exercícios de Lógica/EX7.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int area, lado;
+	cout<<"Digite a area do quadrado: "<<endl;
+	cin>>lado;
+	area = lado*lado;
+	cout<<"Dobro da area = "<<area*2<<endl;
+	return 0;
+}
