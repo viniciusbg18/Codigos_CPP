c32efe0310947c110470b8a09ac22dc64c77e6be
viniciusbg18 <viniciusbaganhagodoy@gmail.com>
2025-04-28 21:44:21 -0300

    Create EX5.cpp

---
+++ Exercícios de Lógica/EX5.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+	float num;
+	cout<<"Digite o numero em centimetros"<<endl;
+	cin>>num;
+	cout<<"Convertido = "<<num*0.01<<" metros";
+	return 0;
+}
