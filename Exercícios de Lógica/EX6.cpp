6b5a3ae23243d5a94ced50a029734ecfc691acab
viniciusbg18 <viniciusbaganhagodoy@gmail.com>
2025-04-28 21:44:55 -0300

    Create EX6.cpp

---
+++ Exercícios de Lógica/EX6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	float raio, pi;
+	pi = 3.14;
+	cout<<"Mostre o valor do Raio do circulo: "<<endl;
+	cin>>raio;
+	cout<<"Area do circulo = "<<pi*raio*raio<<" cm";
+	return 0;
+}
